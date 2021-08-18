b2ec3e597c8c2f2677b54eac2e622c52e422f9fe
Dulya Perera <dulyakemali@gmail.com>
2021-08-18 20:31:56 +0530

    Add dependencies

--- FrontEnd/petsystem/src/App.js
+++ FrontEnd/petsystem/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './components/home';
+import Admin from './components/admin';
+import BoardingPlaceAdmin from './components/admin/BoardingPlace';
+import NewBoardingPlace from './components/admin/BoardingPlace/newPlace';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
 
-export default App;
+  render() {
+    return (
+      <div className='body'>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/admin' component={Admin} />
+            <Route exact path='/admin-boarding-place' component={BoardingPlaceAdmin} />
+            <Route exact path='/new-boarding-place' component={NewBoardingPlace} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    )
+  }
+}
