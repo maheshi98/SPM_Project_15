35d16e364b65a00e21ad0aabeeec4aed139a4841
Maheshi Purnima <mashipurnima1998@gmail.com>
2021-08-18 23:16:34 +0530

    Pet details insert commit

--- FrontEnd/petsystem/src/App.js
+++ FrontEnd/petsystem/src/App.js
@@ -3,8 +3,9 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/home';
 import Admin from './components/admin';
 import BoardingPlaceAdmin from './components/admin/BoardingPlace';
-import NewBoardingPlace from './components/admin/BoardingPlace/newPlace';
+//import NewBoardingPlace from './components/admin/BoardingPlace/newPlace';
 import './App.css';
+import InsertPet from './components/admin/PetDetails/InsertPet';
 
 export default class App extends Component {
 
@@ -16,7 +17,8 @@ export default class App extends Component {
             <Route exact path='/' component={Home} />
             <Route exact path='/admin' component={Admin} />
             <Route exact path='/admin-boarding-place' component={BoardingPlaceAdmin} />
-            <Route exact path='/new-boarding-place' component={NewBoardingPlace} />
+            <Route exact path='/admin-insert-pet' component={InsertPet} />
+            {/* <Route exact path='/new-boarding-place' component={NewBoardingPlace} /> */}
           </Switch>
         </BrowserRouter>
       </div>
