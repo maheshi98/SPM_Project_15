54ce981fb9b28f3842876023a41311cf803743e2
perusha <perushawanniarachchi22@gmail.com>
2021-08-18 21:06:34 +0530

    peru commit3

--- FrontEnd/petsystem/src/App.js
+++ FrontEnd/petsystem/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/home';
 import Admin from './components/admin';
 import BoardingPlaceAdmin from './components/admin/BoardingPlace';
-import NewBoardingPlace from './components/admin/BoardingPlace/newPlace';
+// import NewBoardingPlace from './components/admin/BoardingPlace/newPlace';
 import './App.css';
 
 export default class App extends Component {
@@ -16,7 +16,7 @@ export default class App extends Component {
             <Route exact path='/' component={Home} />
             <Route exact path='/admin' component={Admin} />
             <Route exact path='/admin-boarding-place' component={BoardingPlaceAdmin} />
-            <Route exact path='/new-boarding-place' component={NewBoardingPlace} />
+            {/* <Route exact path='/new-boarding-place' component={NewBoardingPlace} /> */}
           </Switch>
         </BrowserRouter>
       </div>
