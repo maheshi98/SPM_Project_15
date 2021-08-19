c56f8cc4dd5c13303033a42522fd845f17aca0d8
Dulya Perera <dulyakemali@gmail.com>
2021-08-19 15:31:29 +0530

    Integrate with backend

--- FrontEnd/petsystem/src/App.js
+++ FrontEnd/petsystem/src/App.js
@@ -3,10 +3,9 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/home';
 import Admin from './components/admin';
 import BoardingPlaceAdmin from './components/admin/BoardingPlace';
-//import NewBoardingPlace from './components/admin/BoardingPlace/newPlace';
 import VeterinaryService from './components/admin/VeterinaryServices';
 import InsertVeterinay from './components/admin/VeterinaryServices/InsertVeterinaryDetails';
-// import NewBoardingPlace from './components/admin/BoardingPlace/newPlace';
+import NewBoardingPlace from './components/admin/BoardingPlace/insertBoardingPlace';
 import './App.css';
 import InsertPet from './components/admin/PetDetails/InsertPet';
 
@@ -23,7 +22,7 @@ export default class App extends Component {
             <Route exact path='/admin-insert-pet' component={InsertPet} />
             <Route exact path='/admin-veterinary-services' component={VeterinaryService} />
             <Route exact path='/new-veterinary-details' component={InsertVeterinay} />
-            {/* <Route exact path='/new-boarding-place' component={NewBoardingPlace} /> */}
+            <Route exact path='/new-boarding-place' component={NewBoardingPlace} />
           </Switch>
         </BrowserRouter>
       </div>
