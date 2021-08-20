2c8d295e12ac4c03b919d7ceb2ab6d53b645349d
perusha <perushawanniarachchi22@gmail.com>
2021-08-20 17:35:00 +0530

    contact us commit 01

--- FrontEnd/petsystem/src/App.js
+++ FrontEnd/petsystem/src/App.js
@@ -10,8 +10,8 @@ import GenerateReportBoardingPlace from './components/admin/BoardingPlace/genera
 import PetAccessory from './components/admin/PetAccessory';
 import newAccessory from './components/admin/PetAccessory/newAccessory';
 import InsertPet from './components/admin/PetDetails/InsertPet';
-import LogIn from './components/admin/AdminLogin';
-import ContactUs from './components/ContactUs';
+import InsertVeterinay from './components/admin/VeterinaryServices/InsertVeterinaryDetails';
+import VeterinaryService from './components/admin/VeterinaryServices';
 
 export default class App extends Component {
 
