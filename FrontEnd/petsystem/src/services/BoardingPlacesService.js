c482613799a9e7c9baa741c29d41999df4ce2c12
Dulya Perera <dulyakemali@gmail.com>
2021-09-15 08:54:30 +0530

    Add frontend update function

--- FrontEnd/petsystem/src/services/BoardingPlacesService.js
+++ FrontEnd/petsystem/src/services/BoardingPlacesService.js
@@ -10,12 +10,20 @@ class BoardingPlacesService {
         return http.get("/boardingPlace/places");
     }
 
+    get(id) {
+        return http.get(`/boardingPlace/place/${id}`);
+    }
+
     delete(id) {
         return http.delete(`/boardingPlace/delete/${id}`);
     }
 
     getCount() {
         return http.get("/boardingPlace/count");
+    }
+
+    update(id, data) {
+        return http.put(`/boardingPlace/update/${id}`, data);
     }
 
 }
