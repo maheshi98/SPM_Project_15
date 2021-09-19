e6f6e56f46d8d9b909625e661b2defd4d8f7dea6
Dulya Perera <dulyakemali@gmail.com>
2021-09-19 08:22:11 +0530

    Generate report function

--- FrontEnd/petsystem/src/components/admin/BoardingPlace/generateReport/index.jsx
+++ FrontEnd/petsystem/src/components/admin/BoardingPlace/generateReport/index.jsx
@@ -1,9 +1,126 @@
 import React, { Component } from 'react';
+import jsPDF from 'jspdf';
+import "jspdf-autotable";
 import { Row } from 'react-bootstrap';
 import { RiFileDownloadLine } from 'react-icons/ri';
 import './index.css'
 
 export default class GenerateReport extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            people: [
+                { name: "Keanu Reeves", profession: "Actor", image:"https://st.depositphotos.com/1428083/2946/i/600/depositphotos_29460297-stock-photo-bird-cage.jpg" },
+                { name: "Lionel Messi", profession: "Football Player", image: "https://st.depositphotos.com/1428083/2946/i/600/depositphotos_29460297-stock-photo-bird-cage.jpg" },
+                { name: "Cristiano Ronaldo", profession: "Football Player", image: "https://st.depositphotos.com/1428083/2946/i/600/depositphotos_29460297-stock-photo-bird-cage.jpg" },
+                { name: "Jack Nicklaus", profession: "Golf Player", image:"https://st.depositphotos.com/1428083/2946/i/600/depositphotos_29460297-stock-photo-bird-cage.jpg"},
+            ],
+        }
+        this.jsPdfGenerator = this.jsPdfGenerator.bind(this);
+    }
+
+    jsPdfGenerator = () => {
+
+        var doc = new jsPDF();
+
+        // doc.text(50, 50, 'Hello world!')
+
+        // doc.text(20, 20, 'Vehicle details')
+
+        // doc.setFont('helvetica')
+        // doc.text(20, 60, this.state.currentVehicle.vehicleNumber)
+
+
+        // var imgData = '../../../../assets/logo.jpg'
+        // doc.addImage(imgData, 'png', 30, 45, 40, 0)
+        doc.rect(20, 20, doc.internal.pageSize.width - 40, doc.internal.pageSize.height - 40, 'S');
+        doc.setFontSize(11)
+        doc.text(145, 50, 'Beligammana, Mawanella')
+        doc.text(145, 55, '+94 778 357 755')
+        doc.text(145, 60, '+94 352 050 255')
+        doc.text(145, 65, 'cfour@sltnet.lk')
+        doc.setFontSize(20)
+        doc.text(70, 100, 'Vehicle Cost Report')
+        doc.line(65, 102, 140, 102)
+        doc.setFontSize(15)
+        doc.text(40, 130, 'Vehicle Details')
+        doc.setFontSize(12)
+        doc.text(40, 145, 'Vehicle Number:')
+        doc.text(40, 155, 'Vehicle Registered Year:')
+        doc.text(40, 165, 'Vehicle Type:')
+        doc.text(40, 175, 'Vehicle Capacity:')
+        doc.text(40, 185, 'Price*:')
+        doc.text(145, 220, 'Certified By:')
+        doc.text(145, 225, 'Transport Department')
+
+        doc.save(' Report.pdf')
+
+
+    }
+
+    exportPDF = () => {
+        const unit = "pt";
+        const size = "A4"; // Use A1, A2, A3 or A4
+        const orientation = "portrait"; // portrait or landscape
+
+        const marginLeft = 40;
+        const doc = new jsPDF(orientation, unit, size);
+
+        doc.setFontSize(15);
+
+        const title = "My Awesome Report";
+        const headers = [["NAME", "PROFESSION"]];
+
+        const data = this.state.people.map(elt => [elt.name, elt.profession]);
+
+        let content = {
+            startY: 50,
+            head: headers,
+            body: data
+        };
+
+        doc.text(title, marginLeft, 40);
+        doc.autoTable(content);
+        doc.save("report.pdf")
+    }
+
+
+
+
+
+// var doc = new jsPDF('p', 'pt', 'letter');
+// var elem = document.getElementById('tbl-customers');
+// var imgElements = document.querySelectorAll('#tbl-customers tbody img');
+// var data = doc.autoTableHtmlToJson(elem);
+// var images = [];
+// doc.autoTable(data.columns, data.rows, {
+//     bodyStyles: { rowHeight: 30 },
+//     drawCell: function (cell, opts) {
+//         if (opts.column.dataKey === 7) {
+//             var img = imgElements[opts.row.index];
+//             images.push({
+//                 elem: img,
+//                 x: cell.textPos.x,
+//                 y: cell.textPos.y
+//             });
+//         }
+//     },
+//     afterPageContent: function () {
+//         for (var i = 0; i < images.length; i++) {
+//             doc.addImage(images[i].elem, 'jpg', images[i].x, images[i].y);
+//         }
+//     }
+// });
+// doc.save("table.pdf");
+
+
+
+
+
+
+
+
+
     render() {
         return (
             <div className="container" style={{ marginBottom: "5%" }}>
@@ -33,7 +150,8 @@ export default class GenerateReport extends Component {
                     <div className="col-md-4" style={{ marginTop: "5%" }}>
                         <div className="input-group mb-3">
                             <a href="/generate-report-boarding-place">
-                                <button class="member-btn btn"><i><RiFileDownloadLine size="25" /></i> Download</button>
+                                {/* <button class="member-btn btn" onClick={this.jsPdfGenerator}><i><RiFileDownloadLine size="25" /></i> Download</button> */}
+                                <button onClick={() => this.exportPDF()}>Generate Report</button>
                             </a>
                         </div>
                     </div>
@@ -85,11 +203,11 @@ export default class GenerateReport extends Component {
                         </div>
                         <div class="table-cell">
                             <p>
-                                <ol>
+                                {/* <ol>
                                     <li>Extended Stays - LKR 1000/=</li>
                                     <li>Daycare - LKR 1500/=</li>
                                     <li>Grooming - LKR 500/=</li>
-                                </ol>
+                                </ol> */}
                             </p>
                         </div>
                     </div>
