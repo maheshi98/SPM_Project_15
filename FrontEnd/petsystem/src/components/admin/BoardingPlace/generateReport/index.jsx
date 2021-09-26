91dacd83f6f59b019b5906158e1a6effc3540013
Dulya Perera <dulyakemali@gmail.com>
2021-09-26 21:43:28 +0530

    Updates

--- FrontEnd/petsystem/src/components/admin/BoardingPlace/generateReport/index.jsx
+++ FrontEnd/petsystem/src/components/admin/BoardingPlace/generateReport/index.jsx
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import { Row } from 'react-bootstrap';
 import jsPDF from 'jspdf';
 import "jspdf-autotable";
-import BoardingPlaceService from '../../../../Services/BoardingPlacesService'; 
-import {RiFileDownloadLine} from 'react-icons/ri';
+import BoardingPlaceService from '../../../../Services/BoardingPlacesService';
+import { RiFileDownloadLine } from 'react-icons/ri';
 import './index.css'
 
 export default class GenerateReport extends Component {
@@ -12,17 +12,11 @@ export default class GenerateReport extends Component {
         this.retrievePetBoardingPlaces = this.retrievePetBoardingPlaces.bind(this);
         this.onChangeSearchPlace = this.onChangeSearchPlace.bind(this);
         this.searchPetBoardingPlace = this.searchPetBoardingPlace.bind(this);
-        this.jsPdfGenerator = this.jsPdfGenerator.bind(this);
+        // this.jsPdfGenerator = this.jsPdfGenerator.bind(this);
         this.exportPDF = this.exportPDF.bind(this);
         this.state = {
             boardingPlaces: [],
             searchPlace: "",
-            people: [
-                { name: "Keanu Reeves", profession: "Actor", image: "https://st.depositphotos.com/1428083/2946/i/600/depositphotos_29460297-stock-photo-bird-cage.jpg" },
-                { name: "Lionel Messi", profession: "Football Player", image: "https://st.depositphotos.com/1428083/2946/i/600/depositphotos_29460297-stock-photo-bird-cage.jpg" },
-                { name: "Cristiano Ronaldo", profession: "Football Player", image: "https://st.depositphotos.com/1428083/2946/i/600/depositphotos_29460297-stock-photo-bird-cage.jpg" },
-                { name: "Jack Nicklaus", profession: "Golf Player", image: "https://st.depositphotos.com/1428083/2946/i/600/depositphotos_29460297-stock-photo-bird-cage.jpg" },
-            ],
         }
     }
 
@@ -63,69 +57,43 @@ export default class GenerateReport extends Component {
             });
     }
 
-    jsPdfGenerator = () => {
-
-        var doc = new jsPDF();
-
-        // doc.text(50, 50, 'Hello world!')
-
-        // doc.text(20, 20, 'Vehicle details')
-
-        // doc.setFont('helvetica')
-        // doc.text(20, 60, this.state.currentVehicle.vehicleNumber)
-
-
-        // var imgData = '../../../../assets/logo.jpg'
-        // doc.addImage(imgData, 'png', 30, 45, 40, 0)
-        doc.rect(20, 20, doc.internal.pageSize.width - 40, doc.internal.pageSize.height - 40, 'S');
-        doc.setFontSize(11)
-        doc.text(145, 50, 'Beligammana, Mawanella')
-        doc.text(145, 55, '+94 778 357 755')
-        doc.text(145, 60, '+94 352 050 255')
-        doc.text(145, 65, 'cfour@sltnet.lk')
-        doc.setFontSize(20)
-        doc.text(70, 100, 'Vehicle Cost Report')
-        doc.line(65, 102, 140, 102)
-        doc.setFontSize(15)
-        doc.text(40, 130, 'Vehicle Details')
-        doc.setFontSize(12)
-        doc.text(40, 145, 'Vehicle Number:')
-        doc.text(40, 155, 'Vehicle Registered Year:')
-        doc.text(40, 165, 'Vehicle Type:')
-        doc.text(40, 175, 'Vehicle Capacity:')
-        doc.text(40, 185, 'Price*:')
-        doc.text(145, 220, 'Certified By:')
-        doc.text(145, 225, 'Transport Department')
-
-        doc.save(' Report.pdf')
-
-
-    }
 
     exportPDF = () => {
-        const unit = "pt";
-        const size = "A4"; // Use A1, A2, A3 or A4
-        const orientation = "portrait"; // portrait or landscape
 
+        console.log("SSSSSSSSSS")
+        const unit = "pt";
+        const size = "A3"; // Use A1, A2, A3 or A4
+        const orientation = "landscape"; // portrait or landscape
         const marginLeft = 40;
         const doc = new jsPDF(orientation, unit, size);
+        const title = "Pet Boarding Place";
+        const headers = [["Place Image URL", "Place Name", "Place City", "Place Email", "Place Opening Hours", "Services"]];
+        const pet = this.state.boardingPlaces.map(
 
-        doc.setFontSize(15);
-
-        const title = "My Awesome Report";
-        const headers = [["NAME", "PROFESSION"]];
-
-        const data = this.state.people.map(elt => [elt.name, elt.profession]);
-
+            places => [
+                places.placeImage,
+                places.placeName,
+                places.placeCity,
+                places.placeEmail,
+                places.placeOpeningHours,
+                places.placeServices.map(
+                    services => [
+                        services.label,
+                        services.price]
+                )
+            ]
+        );
         let content = {
             startY: 50,
             head: headers,
-            body: data
+            body: pet
         };
 
+        doc.setFontSize(20);
         doc.text(title, marginLeft, 40);
+        require('jspdf-autotable');
         doc.autoTable(content);
-        doc.save("report.pdf")
+        doc.save("Pet Details.pdf");
     }
 
 
@@ -133,7 +101,7 @@ export default class GenerateReport extends Component {
         const { searchPlace } = this.state;
 
         return (
-            <div className="container" style={{marginBottom:"5%"}}>
+            <div className="container" style={{ marginBottom: "5%" }}>
                 <Row>
                     <div class="text-center">
                         <h1 class="head-title">Generate Report For Pet Boarding Places</h1>
